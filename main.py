fb31548397bdf51a7904d0b8b0b278305bd1d506
RondeBoB <morgengaikvissenvangen@hotmail.com>
2025-03-11 21:13:49 +0100

    003 Still does nothing

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def main():
     print("Starting Asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
 
     while True:
         for event in pygame.event.get():
@@ -13,6 +16,7 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
